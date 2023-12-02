fdf679c8b62cd5c42bf6c59c9336989e87e2f6bd
Utkarsh Sharma <122026599+utkarsh007007@users.noreply.github.com>
2023-12-03 01:45:29 +0530

    Add files via upload

---
+++ assets/js/api.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const api_key = "65d59e5ba3bda00ce1dacdfc2adea243";
+const imageBaseURL = "https://image.tmdb.org/t/p/";
+
+// Fetch data from a server using the 'url' and pass the result in JSON data to a 'callback' function along with an optional parameter if has 'optionalParam'
+
+const fetchDataFromServer = function (url, callback, optionalParam) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => callback(data, optionalParam));
+};
+
+export { imageBaseURL, api_key, fetchDataFromServer };
